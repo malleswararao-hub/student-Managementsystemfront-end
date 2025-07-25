fa4a0b62f2a47535b65b2cbbff06c6fd7e921027
malleswararao-hub <malleswararaoduggineni873@gmail.com>
2025-07-25 12:13:35 +0530

    latest

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -7,6 +7,7 @@ import { Student } from './student';
   providedIn: 'root'
 })
 export class StudentService {
+  
 
   baseurl='http://localhost:8080'
   constructor(private http:HttpClient) { }
@@ -14,4 +15,20 @@ export class StudentService {
   getAllStudents():Observable<Student[]>{
    return this.http.get<Student[]>(`${this.baseurl}/getstudents`)
   }
+
+  poststudents(student:Student):Observable<Student>{
+    return this.http.post<Student>(`${this.baseurl}/addstudent`,student)
+  }
+
+  
+
+
+deleteStudent(id: number): Observable<string> {
+  return this.http.delete(`${this.baseurl}/deletestudent?id=${id}`, { responseType: 'text'}
+   
+  ) ;
+}
+
+  
+
 }
